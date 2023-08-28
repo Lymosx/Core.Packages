2d05513691c6fa31d6932f7fc8cd3ce28f2a7b4c
Mehmet Akif <akifdogen@msn.com>
2023-08-28 03:22:50 +0300

    Set 3 - 3.1 : Update

--- Core.Application/Pipelines/Caching/CachingBehavior.cs
+++ Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
 using System.Text;
 using System.Text.Json;
 
@@ -12,9 +13,9 @@ public class CachingBehavior<TRequest, TResponse>
     private readonly CacheSettings _cacheSettings;
     private readonly IDistributedCache _cache;
 
-    public CachingBehavior(CacheSettings cacheSettings, IDistributedCache cache)
+    public CachingBehavior(IDistributedCache cache, IConfiguration configuration)
     {
-        _cacheSettings = cacheSettings;
+        _cacheSettings = configuration.GetSection("CacheSettings").Get<CacheSettings>() ?? throw new InvalidOperationException();
         _cache = cache;
     }
 
@@ -41,7 +42,7 @@ public class CachingBehavior<TRequest, TResponse>
     {
         TResponse response = await next();
         TimeSpan slidingExpiration = request.SlidingExpiration ?? TimeSpan.FromDays(1);
-        DistributedCacheEntryOptions cacheOptions = new() {  SlidingExpiration = slidingExpiration };
+        DistributedCacheEntryOptions cacheOptions = new() { SlidingExpiration = slidingExpiration };
         byte[] serializedData = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
 
         await _cache.SetAsync(request.CacheKey, serializedData, cancellationToken);
